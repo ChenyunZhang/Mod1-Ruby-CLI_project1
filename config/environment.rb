b4bd8e80793d43cbc624cbdb8e4d3a66f72c2ecf
ChenyunZhang <morningchenyun@gmail.com>
2020-08-19 08:25:34 -0400

    environment.rb all set

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 require 'bundler'
 Bundler.require
 
+#environment setting
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/movie_review.db')
 ActiveRecord::Base.logger = nil
 require_all 'app'
